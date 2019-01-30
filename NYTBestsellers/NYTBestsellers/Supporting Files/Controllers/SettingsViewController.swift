878844fb7b1a59b192d427a170267e0c3a5e03b5
Jabeen Cheema <jabeencheema@pursuit.org>
2019-01-29 21:30:57 -0500

    settings picker view is working

--- NYTBestsellers/NYTBestsellers/Supporting Files/Controllers/SettingsViewController.swift
+++ NYTBestsellers/NYTBestsellers/Supporting Files/Controllers/SettingsViewController.swift
@@ -12,15 +12,50 @@ class SettingsViewController: UIViewController {
 
     let settingsview = SettingsView()
     
+    private var settingspicker = [BestsellerGenre]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.settingsview.settingspicker.reloadAllComponents()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         self.view.addSubview(settingsview)
+        settingsview.settingspicker.delegate = self
+        settingsview.settingspicker.dataSource = self 
+        loadGenres()
         
         
     }
     
+    func loadGenres() {
+        APIClient.getGenres { (appError, data) in
+            if let appError = appError {
+                print(appError.errorMessage())
+            } else if let data = data {
+                self.settingspicker = data
+            }
+        }
+    }
+}
 
-   
 
+extension SettingsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return settingspicker.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return settingspicker[row].listName
+    }
+    
+    
+    
 }
